87a5184b20359841ed7323eb64d1c12dc8621e3f
Worldspawn <worldspawn.web@gmail.com>
2023-07-07 21:09:00 +0300

    some logic work

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import http from 'http';
+import path from 'node:path';
 
 const sendFileNames = (fileNames) => {
   const requestBody = JSON.stringify({ clientFiles: fileNames });
@@ -36,19 +37,49 @@ const sendFileNames = (fileNames) => {
   req.end();
 };
 
-const downloadFiles = (fileNames) => {
-  fileNames.forEach((fileName) => {
-    const fileUrl = `http://localhost:3000/files/${fileName}`;
+const downloadFile = (fileUrl, filePath) => {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(filePath);
 
-    http.get(fileUrl, (res) => {
-      const fileStream = fs.createWriteStream(fileName);
-      res.pipe(fileStream);
-    });
+    http
+      .get(fileUrl, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+          file.close();
+          resolve();
+        });
+      })
+      .on('error', (error) => {
+        fs.unlink(filePath, () => {
+          reject(error);
+        });
+      });
   });
 };
 
+const downloadFiles = async (fileNames) => {
+  for (const fileName of fileNames) {
+    const fileUrl = `http://localhost:3000/files/${fileName}`;
+    const filePath = path.join('mods', fileName);
+
+    try {
+      await downloadFile(fileUrl, filePath);
+      console.log(`Downloaded file: ${fileName}`);
+    } catch (error) {
+      console.error(`Error downloading file: ${fileName}`, error);
+    }
+  }
+};
+
 const getFileNamesAndCheck = () => {
-  http.get('http://localhost:3000/files', (res) => {
+  const options = {
+    hostname: 'localhost',
+    port: 3000,
+    path: '/files',
+    method: 'GET',
+  };
+
+  const req = http.get(options, (res) => {
     let responseData = '';
 
     res.on('data', (chunk) => {
@@ -59,7 +90,7 @@ const getFileNamesAndCheck = () => {
       const data = JSON.parse(responseData);
       const fileNames = data.map((file) => file.name);
       const missingFiles = fileNames.filter(
-        (fileName) => !fs.existsSync(fileName)
+        (fileName) => !fs.existsSync(path.join('mods', fileName))
       );
       if (missingFiles.length > 0) {
         downloadFiles(missingFiles);
@@ -68,6 +99,12 @@ const getFileNamesAndCheck = () => {
       }
     });
   });
+
+  req.on('error', (error) => {
+    console.error(error);
+  });
+
+  req.end();
 };
 
 getFileNamesAndCheck();
